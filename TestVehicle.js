5e3d630560b85aa3f003e402952986d9f2339dcd
JackDunne <8jdunne@stpatrick.org>
2017-08-28 14:44:03 -0500

    started working on the things

---
+++ TestVehicle.js
@@ -0,0 +1,11 @@
+const Vehicle = require('./Vehicle.js');
+
+function main(){
+    //make two vehicles and take them for a spin.
+    
+
+
+
+}
+//run the main method
+main();
---
+++ vehicle.js
@@ -0,0 +1,9 @@
+let vehicle = function(e,f,gt,avg,md){
+      let engine = e;
+      let fuel = f;
+      let gastank = gt;
+      let averageMPG = avg;
+      let milesDriven = md;
+}
+
+function totalMiles(){}
